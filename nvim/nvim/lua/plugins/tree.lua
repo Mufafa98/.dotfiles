ef4b5a18a5d6bd43bde4416a75f97e3d1389db50
Mufafa98 <marian.parlea2@gmail.com>
2026-03-25 19:35:06 +0200

    Auto update files for git and autoclose

--- nvim/nvim/lua/plugins/tree.lua
+++ nvim/nvim/lua/plugins/tree.lua
@@ -8,6 +8,9 @@ return {
                     enable = true,
                     ignore = false
                 },
+                filesystem_watchers = {
+                    enable = true,
+                },
                 renderer = {
                     icons = {
                         glyphs = {
@@ -30,6 +33,14 @@ return {
             vim.api.nvim_create_autocmd("VimEnter", {
                 callback = function()
                     require("nvim-tree.api").tree.open()
+                end,
+            })
+            -- auto-close if nvim-tree is the last window
+            vim.api.nvim_create_autocmd("BufEnter", {
+                callback = function()
+                    if vim.fn.winnr("$") == 1 and vim.bo.filetype == "NvimTree" then
+                        vim.cmd("quit")
+                    end
                 end,
             })
         end,
